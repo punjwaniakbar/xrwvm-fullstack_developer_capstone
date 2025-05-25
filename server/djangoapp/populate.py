6aa18571f5e3a464e44d69b0f51025967b6b22eb
Akbar Punjwani <akbarpunjwani@yahoo.com>
2025-05-25 15:14:12 -0400

    djangoapp model populate restapis urls and views modified for pylint errors

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -23,7 +23,7 @@ def initiate():
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV",
-       "year": 2023, "car_make":c ar_make_instances[0]},
+       "year": 2023, "car_make": car_make_instances[0]},
       {"name": "Qashqai", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[0]},
       {"name": "XTRAIL", "type": "SUV",
